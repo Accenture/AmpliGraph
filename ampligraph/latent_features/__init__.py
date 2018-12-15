bdbc36aab7b43bb694dbec5c6133040142df0364
Sumit Pai <spai@dalgpu004.techlabs.accenture.com>
2018-12-15 02:24:17 +0000

    Added L1 and L2 regularization

--- ampligraph/latent_features/__init__.py
+++ ampligraph/latent_features/__init__.py
@@ -4,9 +4,10 @@ from .models import EmbeddingModel, TransE, DistMult, ComplEx, RandomBaseline
 from .loss_functions import pairwise_loss, negative_log_likelihood_loss, absolute_margin_loss
 from .misc import get_entity_triples
 from .model_utils import save_model, restore_model
+from .regularizers import l1_regularizer, l2_regularizer
 
 __all__ = ['EmbeddingModel', 'TransE', 'DistMult', 'ComplEx', 'RandomBaseline',
            'pairwise_loss', 'negative_log_likelihood_loss', 'absolute_margin_loss', 'get_entity_triples',
-           'save_model', 'restore_model']
+           'save_model', 'restore_model', 'l1_regularizer', 'l2_regularizer']
 
 
