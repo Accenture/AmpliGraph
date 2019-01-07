91044596a0444b7941973cb798c06e7cb32fcd1c
Sumit Pai <spai@dalgpu004.techlabs.accenture.com>
2019-01-07 20:00:31 +0000

    Added adverserial loss - refer RotatE

--- ampligraph/latent_features/__init__.py
+++ ampligraph/latent_features/__init__.py
@@ -1,13 +1,13 @@
 """This module includes neural graph embedding models and support functions (such as loss functions)."""
 
 from .models import EmbeddingModel, TransE, DistMult, ComplEx, RandomBaseline
-from .loss_functions import PairwiseLoss, NLLLoss, AbsoluteMarginLoss
+from .loss_functions import PairwiseLoss, NLLLoss, AbsoluteMarginLoss, SelfAdverserialLoss
 from .misc import get_entity_triples
 from .model_utils import save_model, restore_model
 from .regularizers import l1_regularizer, l2_regularizer
 
 __all__ = ['EmbeddingModel', 'TransE', 'DistMult', 'ComplEx', 'RandomBaseline',
-           'PairwiseLoss', 'NLLLoss', 'AbsoluteMarginLoss', 'get_entity_triples',
+           'PairwiseLoss', 'NLLLoss', 'AbsoluteMarginLoss', 'SelfAdverserialLoss', 'get_entity_triples',
            'save_model', 'restore_model', 'l1_regularizer', 'l2_regularizer']
 
 
