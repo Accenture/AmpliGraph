Fixed #10 and #7; Fixed other issue: normalization of Complex Entity Embeddings and sizes, Prediction for single triple, Early stopping with filter, Print messages

ampligraph/evaluation/protocol.py
@@ -290,6 +290,8 @@ def to_idx(X, ent_to_idx=None, rel_to_idx=None):
     X : ndarray, shape [n, 3]
         The ndarray of converted statements.
     """
+    if X.ndim==1:
+        X = X[np.newaxis,:]
     x_idx_s = np.vectorize(ent_to_idx.get)(X[:, 0])
     x_idx_p = np.vectorize(rel_to_idx.get)(X[:, 1])
     x_idx_o = np.vectorize(ent_to_idx.get)(X[:, 2])
@@ -421,58 +423,62 @@ def gridsearch_next_hyperparam(model_name, in_dict):
     out_dict: dictionary containing an instance of model hypermeters
     """
     from ..latent_features import LOSS_REGISTRY, REGULARIZER_REGISTRY, MODEL_REGISTRY
-    for batch_count in in_dict["batches_count"]:
-        for epochs in in_dict["epochs"]:
-            for k in in_dict["k"]:
-                for eta in in_dict["eta"]:
-                    for reg_type, reg_params, reg_param_values in \
-                        yield_all_permutations(REGULARIZER_REGISTRY, in_dict["regularizer"], in_dict["regularizer_params"]):
-                        for optimizer_type in in_dict["optimizer"]:
-                            for optimizer_lr in in_dict["optimizer_params"]["lr"]:
-                                for loss_type, loss_params, loss_param_values in \
-                                    yield_all_permutations(LOSS_REGISTRY, in_dict["loss"], in_dict["loss_params"]):
-                                    for model_type, model_params, model_param_values in \
-                                        yield_all_permutations(MODEL_REGISTRY, [model_name], in_dict["embedding_model_params"]):
-                                        
-                                        try:
-                                            verbose = in_dict["verbose"]
-                                        except KeyError:
-                                            verbose = False
-                                            
-                                        try:
-                                            seed = in_dict["seed"]
-                                        except KeyError:
-                                            seed = -1
-                                            
-                                        out_dict = {
-                                            "batches_count": batch_count,
-                                            "epochs": epochs,
-                                            "k": k,
-                                            "eta": eta,
-                                            "loss": loss_type,
-                                            "loss_params": {},
-                                            "embedding_model_params": {},
-                                            "regularizer": reg_type,
-                                            "regularizer_params": {},
-                                            "optimizer": optimizer_type,
-                                            "optimizer_params":{
-                                                "lr": optimizer_lr
-                                                },
-                                            "verbose": verbose
-                                            }
-                                
-                                        if seed >= 0:
-                                            out_dict["seed"] = seed
-                                            
-                                        for idx in range(len(loss_params)):
-                                            out_dict["loss_params"][loss_params[idx]] = loss_param_values[idx]
-                                        for idx in range(len(reg_params)):
-                                            out_dict["regularizer_params"][reg_params[idx]] = reg_param_values[idx]
-                                        for idx in range(len(model_params)):
-                                            out_dict["embedding_model_params"][model_params[idx]] = model_param_values[idx] 
-                                            
-                                        yield (out_dict)
-                                        
+    try:
+        for batch_count in in_dict["batches_count"]:
+            for epochs in in_dict["epochs"]:
+                for k in in_dict["k"]:
+                    for eta in in_dict["eta"]:
+                        for reg_type, reg_params, reg_param_values in \
+                            yield_all_permutations(REGULARIZER_REGISTRY, in_dict["regularizer"], in_dict["regularizer_params"]):
+                            for optimizer_type in in_dict["optimizer"]:
+                                for optimizer_lr in in_dict["optimizer_params"]["lr"]:
+                                    for loss_type, loss_params, loss_param_values in \
+                                        yield_all_permutations(LOSS_REGISTRY, in_dict["loss"], in_dict["loss_params"]):
+                                        for model_type, model_params, model_param_values in \
+                                            yield_all_permutations(MODEL_REGISTRY, [model_name], in_dict["embedding_model_params"]):
+
+                                            try:
+                                                verbose = in_dict["verbose"]
+                                            except KeyError:
+                                                verbose = False
+
+                                            try:
+                                                seed = in_dict["seed"]
+                                            except KeyError:
+                                                seed = -1
+
+                                            out_dict = {
+                                                "batches_count": batch_count,
+                                                "epochs": epochs,
+                                                "k": k,
+                                                "eta": eta,
+                                                "loss": loss_type,
+                                                "loss_params": {},
+                                                "embedding_model_params": {},
+                                                "regularizer": reg_type,
+                                                "regularizer_params": {},
+                                                "optimizer": optimizer_type,
+                                                "optimizer_params":{
+                                                    "lr": optimizer_lr
+                                                    },
+                                                "verbose": verbose
+                                                }
+
+                                            if seed >= 0:
+                                                out_dict["seed"] = seed
+
+                                            for idx in range(len(loss_params)):
+                                                out_dict["loss_params"][loss_params[idx]] = loss_param_values[idx]
+                                            for idx in range(len(reg_params)):
+                                                out_dict["regularizer_params"][reg_params[idx]] = reg_param_values[idx]
+                                            for idx in range(len(model_params)):
+                                                out_dict["embedding_model_params"][model_params[idx]] = model_param_values[idx] 
+
+                                            yield (out_dict)
+    except KeyError as e:
+        print('One or more of the hyperparameters was not passed:')
+        print(str(e))
+
 
 
 

ampligraph/latent_features/loss_functions.py
@@ -23,7 +23,7 @@ class Loss(abc.ABC):
     external_params = []
     class_params = {}
     
-    def __init__(self, eta, hyperparam_dict):
+    def __init__(self, eta, hyperparam_dict, verbose=False):
         """Initialize Loss
 
         Parameters
@@ -40,11 +40,12 @@ class Loss(abc.ABC):
         try:
             self._loss_parameters['eta'] = eta
             self._init_hyperparams(hyperparam_dict)
-            print('------ Loss-----')
-            print('Name:', self.name)
-            print('Parameters:')
-            for key in self._loss_parameters.keys():
-                print("  ", key, ": ", self._loss_parameters[key])
+            if verbose:
+                print('------ Loss-----')
+                print('Name:', self.name)
+                print('Parameters:')
+                for key in self._loss_parameters.keys():
+                    print("  ", key, ": ", self._loss_parameters[key])
             
         except KeyError:
             raise Exception('Some of the hyperparams for loss were not passed to the loss function')
@@ -144,8 +145,8 @@ class PairwiseLoss(Loss):
 
     """
     
-    def __init__(self, eta, hyperparam_dict):
-        super().__init__(eta,  hyperparam_dict)
+    def __init__(self, eta, hyperparam_dict, verbose=False):
+        super().__init__(eta,  hyperparam_dict, verbose)
         
     def _init_hyperparams(self, hyperparam_dict):
         """ Verifies and stores the hyperparams needed by the algorithm
@@ -178,8 +179,8 @@ class NLLLoss(Loss):
     :math:`\mathcal{C}` is the set of corruptions, :math:`f_{model}(t;\Theta)` is the model-specific scoring function.
 
     """
-    def __init__(self, eta, hyperparam_dict):
-        super().__init__(eta, hyperparam_dict)
+    def __init__(self, eta, hyperparam_dict, verbose=False):
+        super().__init__(eta, hyperparam_dict, verbose)
         
     
     def _init_hyperparams(self, hyperparam_dict):
@@ -225,8 +226,8 @@ class AbsoluteMarginLoss(Loss):
        :math:`\mathcal{C}` is the set of corruptions, :math:`f_{model}(t;\Theta)` is the model-specific scoring function.
     """
     
-    def __init__(self, eta, hyperparam_dict):
-        super().__init__(eta, hyperparam_dict)
+    def __init__(self, eta, hyperparam_dict, verbose=False):
+        super().__init__(eta, hyperparam_dict, verbose)
         
     def _init_hyperparams(self, hyperparam_dict):
         """ Verifies and stores the hyperparams needed by the algorithm
@@ -271,8 +272,8 @@ class SelfAdverserialLoss(Loss):
 
        where :math:`\gamma` is the margin, and p(h_{i}^{'} ,r,t_{i}^{'} ) is the sampling proportion
     """
-    def __init__(self, eta, hyperparam_dict):
-        super().__init__(eta, hyperparam_dict)
+    def __init__(self, eta, hyperparam_dict, verbose=False):
+        super().__init__(eta, hyperparam_dict, verbose)
     
     def _init_hyperparams(self, hyperparam_dict):
         """ Verifies and stores the hyperparams needed by the algorithm
@@ -314,3 +315,4 @@ class SelfAdverserialLoss(Loss):
                                 tf.reduce_sum(tf.multiply(p_neg, \
                                                           tf.log( tf.nn.sigmoid( tf.negative(scores_neg_reshaped) - margin)) ))
         return loss
+

ampligraph/latent_features/models.py
@@ -115,12 +115,12 @@ class EmbeddingModel(abc.ABC):
                   'This may introduce memory issues.')
             
         try:
-            self.loss = LOSS_REGISTRY[loss](self.eta, self.loss_params)
+            self.loss = LOSS_REGISTRY[loss](self.eta, self.loss_params, verbose=verbose)
         except KeyError:
             raise ValueError('Unsupported loss function: %s' % loss)
             
         try:
-            self.regularizer = REGULARIZER_REGISTRY[regularizer](self.regularizer_params)
+            self.regularizer = REGULARIZER_REGISTRY[regularizer](self.regularizer_params, verbose=verbose)
         except KeyError:
             raise ValueError('Unsupported regularizer: %s' % regularizer)
             
@@ -298,12 +298,15 @@ class EmbeddingModel(abc.ABC):
 
         self.sess_train = tf.Session(config=self.tf_config)
 
+        batch_size = X.shape[0]//self.batches_count
+        dataset = tf.data.Dataset.from_tensor_slices(X).repeat().batch(batch_size).prefetch(2)
+        dataset_iter = dataset.make_one_shot_iterator()
         # init tf graph/dataflow for training
         # init variables (model parameters to be learned - i.e. the embeddings)
         self._initialize_embeddings()
 
         # training input placeholder
-        x_pos_tf = tf.placeholder(tf.int32, shape=[None, 3]) 
+        x_pos_tf = tf.cast(dataset_iter.get_next(), tf.int32)
         all_ent_tf = tf.squeeze(tf.constant(list(self.ent_to_idx.values()), dtype=tf.int32))
         #generate negatives
         x_neg_tf = generate_corruptions_for_fit(x_pos_tf, all_ent_tf, self.eta, rnd=self.seed)
@@ -329,7 +332,6 @@ class EmbeddingModel(abc.ABC):
         
         #early stopping 
         if early_stopping:
-            print('Enabled Early Stopping')
             try:
                 x_valid = early_stopping_params['x_valid']
                 if type(x_valid) != np.ndarray:
@@ -347,43 +349,41 @@ class EmbeddingModel(abc.ABC):
             if early_stopping_criteria not in ['hits10','hits1', 'hits3', 'mrr']:
                 raise ValueError('Unsupported early stopping criteria')
             
-            self._initialize_eval_graph()                     
+                             
             early_stopping_best_value = 0
             early_stopping_stop_counter = 0
             try:
                 x_filter = early_stopping_params['x_filter']
                 x_filter = to_idx(x_filter, ent_to_idx=self.ent_to_idx, rel_to_idx=self.rel_to_idx)
-
-                print('Setting filter for early stopping')
                 self.set_filter_for_eval(x_filter)
             except KeyError:
-                print('No filter for early stopping')
                 pass
             
+            self._initialize_eval_graph()    
             
         
         self.sess_train.run(tf.tables_initializer())
         self.sess_train.run(tf.global_variables_initializer())
         
-        X_batches = np.array_split(X, self.batches_count)
+        #X_batches = np.array_split(X, self.batches_count)
         
             
         
         for epoch in tqdm(range(self.epochs), disable=(not self.verbose), unit='epoch'):
             losses = []
-            for j in range(self.batches_count):
-                X_pos_b = X_batches[j]
-                _, loss_batch = self.sess_train.run([train, loss], {x_pos_tf: X_pos_b})
-                self.sess_train.run(normalize_ent_emb_op)
-                if self.verbose:
-                    mean_loss = loss_batch / (len(X_pos_b)*self.eta)
-                    losses.append(mean_loss)
-                    tqdm.write('epoch: %d, batch %d: mean loss: %.10f' % (epoch, j, np.mean(np.array(losses))))
+            for batch in range(self.batches_count):
+                loss_batch, _ = self.sess_train.run([ loss, train])#, {x_pos_tf: X_pos_b})
+                if self.embedding_model_params.get('normalize_entity_embd', False):
+                    self.sess_train.run(normalize_ent_emb_op)
+
+                mean_loss = loss_batch/self.eta
+                losses.append(mean_loss)
+             
+            tqdm.write('epoch: %d: mean loss: %.10f' % (epoch, np.mean(np.array(losses))/ (batch_size)))
             
             # TODO TEC-1529: add early stopping criteria
-            if early_stopping and epoch >= early_stopping_params.get('burn_in', 5)  \
-                                and epoch%early_stopping_params.get('check_interval',3)==0:
-                #print('Early Stopping test epoch:', epoch)
+            if early_stopping and epoch >= early_stopping_params.get('burn_in', 100)  \
+                                and epoch%early_stopping_params.get('check_interval',10)==0:
                 #compute and store test_loss
                 ranks = []
                 if x_valid.ndim>1:
@@ -393,7 +393,6 @@ class EmbeddingModel(abc.ABC):
                 else:
                     ranks = self.sess_train.run([self.rank], feed_dict={self.X_test_tf: [x_test_triple]})
                 
-                #print('Early Stopping Best ' + early_stopping_criteria +' :', early_stopping_best_value)
                 if early_stopping_criteria == 'hits10':
                     current_test_value = hits_at_n_score(ranks,10)
                 elif early_stopping_criteria == 'hits3':
@@ -402,20 +401,16 @@ class EmbeddingModel(abc.ABC):
                     current_test_value = hits_at_n_score(ranks,1)
                 elif early_stopping_criteria == 'mrr':
                     current_test_value = mrr_score(ranks)
-                #print('Early Stopping Current ' + early_stopping_criteria +' :', current_test_value)
                 
                 
                 if early_stopping_best_value >= current_test_value:
                     early_stopping_stop_counter += 1
-                    #print('Found Worse', early_stopping_stop_counter)
                     if early_stopping_stop_counter == early_stopping_params.get('stop_interval', 3):
-                        print('Early stopping with best model ' + early_stopping_criteria +' :', early_stopping_best_value)
                         self.is_filtered = False
                         self.sess_train.close()
                         self.is_fitted = True
                         return
                 else:
-                    #print('Found Better')
                     early_stopping_best_value = current_test_value
                     early_stopping_stop_counter = 0
                     self._save_trained_params()
@@ -662,7 +657,7 @@ class RandomBaseline():
     def predict(self, X, from_idx=False):
         return self.rnd.uniform(low=0, high=1, size=len(X))
 
-@register_model("TransE", ["norm"])
+@register_model("TransE", ["norm", "normalize_entity_embd"])
 class TransE(EmbeddingModel):
     """The Translating Embedding model (TransE)
 
@@ -765,7 +760,7 @@ class TransE(EmbeddingModel):
         """
         return super().predict(X, from_idx=from_idx)
 
-@register_model("DistMult")
+@register_model("DistMult", ["normalize_entity_embd"])
 class DistMult(EmbeddingModel):
     """The DistMult model.
 
@@ -921,6 +916,15 @@ class ComplEx(EmbeddingModel):
                          regularizer=regularizer, regularizer_params =regularizer_params,
                          model_checkpoint_path=model_checkpoint_path, verbose=verbose, **kwargs)
 
+    def _initialize_embeddings(self):
+        """ Initialize the embeddings
+        """
+        self.ent_emb = tf.get_variable('ent_emb', shape=[len(self.ent_to_idx), self.k*2],
+                                        initializer=self.initializer)
+        self.rel_emb = tf.get_variable('rel_emb', shape=[len(self.rel_to_idx), self.k*2],
+                                        initializer=self.initializer)
+        
+        
     def _fn(self, e_s, e_p, e_o):
         """The ComplEx scoring function.
 