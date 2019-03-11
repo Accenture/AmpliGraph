930ad1e4fe222c8e204ead82225c01325f176f81
Costabello, Luca <luca.costabello@accenture.com>
2019-03-11 16:33:20 +0000

    re-enabled logging dependency in sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = [
 ]
 
 
-autodoc_mock_imports = ["tensorflow", 'logging']
+autodoc_mock_imports = ["tensorflow"]
 
 
 # Add any paths that contain templates here, relative to this directory.
