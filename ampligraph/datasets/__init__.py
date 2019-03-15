a557fc613defd7748c37af569bdfa85a266b02eb
Rory McGrath <rory.m.mc.grath@accenture.com>
2019-03-15 11:22:24 +0000

    Removed references to wn11 and fb13 that were blocking sphinx document genreation. #55

--- ampligraph/datasets/__init__.py
+++ ampligraph/datasets/__init__.py
@@ -3,5 +3,5 @@
 from .datasets import load_from_csv, load_from_rdf, load_fb15k, load_wn18, load_fb15k_237, load_from_ntriples, \
     load_yago3_10, load_wn18rr
 
-__all__ = ['load_from_csv', 'load_from_rdf', 'load_from_ntriples', 'load_wn11', 'load_wn18', 'load_fb15k',
-           'load_fb15k_237', 'load_fb13', 'load_yago3_10', 'load_wn18rr']
+__all__ = ['load_from_csv', 'load_from_rdf', 'load_from_ntriples', 'load_wn18', 'load_fb15k',
+           'load_fb15k_237',  'load_yago3_10', 'load_wn18rr']
