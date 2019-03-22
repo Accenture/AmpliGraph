ec151c230cec7eaa5abcf222f918bba493834959
Le Van, Chan V <chan.v.le.van@accenture.com>
2019-03-22 14:54:10 +0000

    moving clean unseen func to datasets.py, excluding unseen entities in wn18rr and fb15k-237 right after loaded

--- tests/ampligraph/datasets/test_datasets.py
+++ tests/ampligraph/datasets/test_datasets.py
@@ -38,31 +38,15 @@ def test_load_fb15k():
 
 def test_load_fb15k_237():
     fb15k_237 = load_fb15k_237()
-    assert len(fb15k_237['train']) == 272115
-    assert len(fb15k_237['valid']) == 17535
-    assert len(fb15k_237['test']) == 20466
+    assert len(fb15k_237['train']) == 272115 
+    
+    # - 9 because 9 triples containing unseen entities are removed
+    assert len(fb15k_237['valid']) == 17535 - 9
 
-    # ent_train = np.union1d(np.unique(fb15k_237["train"][:,0]), np.unique(fb15k_237["train"][:,2]))
-    # ent_valid = np.union1d(np.unique(fb15k_237["valid"][:,0]), np.unique(fb15k_237["valid"][:,2]))
-    # ent_test = np.union1d(np.unique(fb15k_237["test"][:,0]), np.unique(fb15k_237["test"][:,2]))
-    # distinct_ent = np.union1d(np.union1d(ent_train, ent_valid), ent_test)
-    # distinct_rel = np.union1d(np.union1d(np.unique(fb15k_237["train"][:,1]), np.unique(fb15k_237["train"][:,1])), np.unique(fb15k_237["train"][:,1]))
+    # - 28 because 28 triples containing unseen entities are removed
+    assert len(fb15k_237['test']) == 20466 - 28
 
-    # assert len(distinct_ent) == 14541  
-    # assert len(distinct_rel) == 237     
-
-
-    # train_all_ent = set(fb15k_237['train'].flatten())
-    # valid_all_ent = set(fb15k_237['valid'].flatten())
-    # test_all_ent = set(fb15k_237['test'].flatten())
-
-    # unseen_valid = valid_all_ent - train_all_ent
-    # train_valid_ent = (valid_all_ent - unseen_valid) | train_all_ent
-
-    # unseen_test = test_all_ent - train_valid_ent
-
-    # assert len(unseen_valid) == 8
-    # assert len(unseen_test) == 29
+   
 
 
 def test_yago_3_10():
@@ -92,19 +76,10 @@ def test_wn18rr():
                               np.unique(wn18rr["train"][:, 1]))
 
     assert len(wn18rr['train']) == 86835
-    assert len(wn18rr['valid']) == 3034
-    assert len(wn18rr['test']) == 3134
-    # assert len(distinct_ent) == 40943
-    # assert len(distinct_rel) == 11
 
-    # train_all_ent = set(wn18rr['train'].flatten())
-    # valid_all_ent = set(wn18rr['valid'].flatten())
-    # test_all_ent = set(wn18rr['test'].flatten())
+    # - 210 because 210 triples containing unseen entities are removed
+    assert len(wn18rr['valid']) == 3034 - 210
 
-    # unseen_valid = valid_all_ent - train_all_ent
-    # train_valid_ent = (valid_all_ent - unseen_valid) | train_all_ent
-
-    # unseen_test = test_all_ent - train_valid_ent
-
-    # assert len(unseen_valid) == 198
-    # assert len(unseen_test) == 209
+    # - 210 because 210 triples containing unseen entities are removed
+    assert len(wn18rr['test']) == 3134 - 210
+    
