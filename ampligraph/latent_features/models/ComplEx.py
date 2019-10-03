ce27b0434c0d235c0bebc76404d78c027420e274
tabacof <tabacof@gmail.com>
2019-10-03 15:26:20 +0100

    Fixing linting

--- ampligraph/latent_features/models/ComplEx.py
+++ ampligraph/latent_features/models/ComplEx.py
@@ -16,9 +16,10 @@ class ComplEx(EmbeddingModel):
 
         f_{ComplEx}=Re(\langle \mathbf{r}_p, \mathbf{e}_s, \overline{\mathbf{e}_o}  \rangle)
 
-    ComplEx can be improved if used alongside the nuclear 3-norm (the **ComplEx-N3** model :cite:`lacroix2018canonical`),
-    which can be easily added to the loss function via the ``regularizer`` hyperparameter with ``p=3``
-    and a chosen regularisation weight (represented by ``lambda``), as shown in the example below.
+    ComplEx can be improved if used alongside the nuclear 3-norm
+    (the **ComplEx-N3** model :cite:`lacroix2018canonical`), which can be easily added to the
+    loss function via the ``regularizer`` hyperparameter with ``p=3``and
+    a chosen regularisation weight (represented by ``lambda``), as shown in the example below.
     See also :meth:`ampligraph.latent_features.LPRegularizer`.
 
     .. note::
