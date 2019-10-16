fa6c90b91a113147af33e4aa255d6102dc3b930b
nicholas.mccarthy <nicholas.mccarthy@accenture.com>
2019-10-16 17:06:08 +0100

    Lint.

--- ampligraph/latent_features/models/ConvKB.py
+++ ampligraph/latent_features/models/ConvKB.py
@@ -9,7 +9,7 @@ import numpy as np
 import tensorflow as tf
 import logging
 
-from .EmbeddingModel import EmbeddingModel, register_model ,ENTITY_THRESHOLD
+from .EmbeddingModel import EmbeddingModel, register_model, ENTITY_THRESHOLD
 from ..initializers import DEFAULT_XAVIER_IS_UNIFORM
 from ampligraph.latent_features import constants as constants
 
