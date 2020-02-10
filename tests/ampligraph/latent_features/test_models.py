51d4b10f1609d3890cef7337ea096434533bb4a3
sumitpai <sumit.pai@accenture.com>
2020-02-10 17:09:07 +0000

    Added unit test case for #174

--- tests/ampligraph/latent_features/test_models.py
+++ tests/ampligraph/latent_features/test_models.py
@@ -11,7 +11,7 @@ import os
 
 from ampligraph.latent_features import TransE, DistMult, ComplEx, HolE, RandomBaseline, ConvKB
 from ampligraph.latent_features import set_entity_threshold, reset_entity_threshold
-from ampligraph.datasets import load_wn18
+from ampligraph.datasets import load_wn18, load_wn18rr
 from ampligraph.utils import save_model, restore_model
 from ampligraph.evaluation import evaluate_performance, hits_at_n_score
 from ampligraph.evaluation.protocol import to_idx
@@ -29,6 +29,34 @@ def test_large_graph_mode():
 
     y = model.predict(X['test'][:1])
     print(y)
+    reset_entity_threshold()
+
+
+def test_output_sizes():
+    ''' Test to check whether embedding matrix sizes match the input data (num rel/ent and k)
+    '''
+    def perform_test():
+        X = load_wn18rr()
+        k = 5
+        unique_entities = np.unique(np.concatenate([X['train'][:, 0], 
+                                                    X['train'][:, 2]], 0))
+        unique_relations = np.unique(X['train'][:, 1])
+        model = TransE(batches_count=100, seed=555, epochs=1, k=k, loss='multiclass_nll', loss_params={'margin': 5},
+                        verbose=True, optimizer='sgd', optimizer_params={'lr': 0.001})
+        model.fit(X['train'])
+        # verify ent and rel shapes
+        assert(model.trained_model_params[0].shape[0] == len(unique_entities))
+        assert(model.trained_model_params[1].shape[0] == len(unique_relations))
+        # verify k
+        assert(model.trained_model_params[0].shape[1] == k)
+        assert(model.trained_model_params[1].shape[1] == k)
+
+    # Normal mode
+    perform_test()
+
+    # Large graph mode
+    set_entity_threshold(10)
+    perform_test()
     reset_entity_threshold()
 
 
