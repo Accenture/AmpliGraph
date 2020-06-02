da7b7f6139a982b8486ef3ec85e7fe7872a902d8
sumitpai <sumit.pai@accenture.com>
2020-06-02 17:46:29 +0100

    added demo script for multi gpu inference

--- ampligraph/latent_features/models/EmbeddingModel.py
+++ ampligraph/latent_features/models/EmbeddingModel.py
@@ -10,9 +10,9 @@ from ampligraph.latent_features.layers.corruption_generation import CorruptionGe
 
 
 class EmbeddingModel(tf.keras.Model):
-    def __init__(self, eta, ent_to_idx, rel_to_idx, algo='DistMult'):
+    def __init__(self, eta, ent_to_idx, rel_to_idx, algo='DistMult', seed=0):
         super(EmbeddingModel, self).__init__()
-        
+        tf.random.set_seed(seed)
         self.corruption_layer = CorruptionGenerationLayerTrain(eta, len(ent_to_idx))
         self.corruption_layer_eval = CorruptionGenerationLayerEval(len(ent_to_idx), len(ent_to_idx))
         self.encoding_layer = EmbeddingLookupLayer(len(ent_to_idx), len(rel_to_idx), 300)
