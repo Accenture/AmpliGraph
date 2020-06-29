1267c3f3c5a1ed328bedb0e2e61e077540050b1b
Adrianna Janik <adrianna.janik@accenture.com>
2020-06-29 12:35:56 +0100

    Added comments to GraphDataLoader, DataSourceIdentifier and SQLiteAdapter. Minor changes.

--- ampligraph/datasets/graph_data_loader.py
+++ ampligraph/datasets/graph_data_loader.py
@@ -6,28 +6,37 @@
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 from ampligraph.datasets.source_identifier import DataSourceIdentifier
-import sqlite3
-from sqlite3 import Error
-import numpy as np
-from urllib.request import pathname2url
-import os
-import shelve
-import pandas as pd
 from datetime import datetime
-from ampligraph.utils.profiling import get_human_readable_size
+
 
 class DummyBackend():
+"""Class providing artificial backend, that reads data into memory."""
     def __init__(self, identifier, verbose=False):
+    """Initialise DummyBackend.
+
+       Parameters
+       ----------
+       identifier: initialize data source identifier, provides loader. 
+    """
         self.verbose = verbose
         self.identifier = identifier
         
     def __enter__ (self):
+    """Context manager enter function. Required by GraphDataLoader."""
         return self
     
     def __exit__ (self, type, value, tb):
+    """Context manager exit function. Required by GraphDataLoader."""
         pass
     
     def _load(self, data_source, dataset_type):
+    """Loads data into self.data.
+       
+       Parameters
+       ----------
+       data_source: file with data.
+       dataset_type: kind of data to be loaded (train | test | validation).
+    """
         if self.verbose:
             print("Simple in-memory data loading of {} dataset.".format(dataset_type))
         self.data_source = data_source
@@ -35,6 +44,19 @@ class DummyBackend():
         self.data = loader(data_source)
         
     def _get_complementary_entities(self, triple):
+    """Get subjects and objects complementary to a triple (?,p,?).
+       Returns the participating entities in the relation ?-p-o and s-p-?.
+
+       Parameters
+       ----------
+       x_triple: nd-array (3,)
+           triple (s-p-o) that we are querying.
+
+       Returns
+       -------
+       entities: list of entities participating in the relations s-p-? and ?-p-o.
+   """
+
         if self.verbose:        
             print("Getting complementary entities")
         entities = self._get_complementary_subjects(triple)
@@ -42,6 +64,18 @@ class DummyBackend():
         return list(set(entities))
 
     def _get_complementary_subjects(self, triple):
+    """Get subjects complementary to a triple (?,p,o).
+       For a given triple retrive all triples whith same objects and predicates.
+
+       Parameters
+       ----------
+       triple: list or array with 3 elements (subject, predicate, object).
+
+       Returns
+       -------
+       result of a query, list of subjects.
+    """
+
         if self.verbose:        
             print("Getting complementary subjects")
         subjects = self.data[self.data[:,2] == triple[2]]
@@ -49,6 +83,17 @@ class DummyBackend():
         return subjects
 
     def _get_complementary_objects(self, triple):
+    """Get objects complementary to a triple (s,p,?).
+       For a given triple retrive all triples whith same subjects and predicates.
+
+       Parameters
+       ----------
+       triple: list or array with 3 elements (subject, predicate, object).
+
+       Returns
+       -------
+       result of a query, list of objects.
+    """
         if self.verbose:        
             print("Getting complementary objects")
         objects = self.data[self.data[:,0] == triple[0]]
@@ -56,19 +101,47 @@ class DummyBackend():
         return objects
         
     def _get_batch(self, batch_size, dataset_type="train"):
+    """Get next btch of data (generator).
+    
+       Parameters
+       ----------
+       batch_size: size of a batch
+       dataset_type: kind of dataset that is needed (train | test | validation).
+
+       Returns
+       --------
+       ndarray(batch_size, 3)
+    """
         length = len(self.data)
         for ind in range(0, length, batch_size):
             yield self.data[ind:min(ind + batch_size, length)]
 
 class GraphDataLoader():
-    """Desiderata:
-        - batch iterator
-        - some other functions graph-specific get complementary entities
-          previously called "participating entities"
+    """Data loader for graphs implemented as a batch iterator
+        - graph-specific functions: get complementary entities
+          (previously called "participating entities"),
         - support for various backends and in-memory processing through dependency injection
+
+       Example
+       -------
+       >>># with a SQLite backend
+       >>>data = GraphDataLoader("./train.csv", backend=SQLiteAdapter, batch_size=4)
+       >>># with no backend
+       >>>data = GraphDataLoader("./train.csv", batch_size=4)
+       >>>for elem in data:
+       >>>    process(data)
     """    
     def __init__(self, data_source, batch_size=8, dataset_type="train", backend=None, verbose=False):
-        """Initialize persistent/in-memory data storage."""   
+        """Initialise persistent/in-memory data storage.
+       
+           Parameters
+           ----------
+           data_source: file with data (e.g. CSV).
+           batch_size: size of batch,
+           dataset_type: kind of data provided (train | test | validation),
+           backend: name of backend class or, already initialised backend, 
+                    if None, DummyBackend is used (in-memory processing).
+        """   
         self.dataset_type = dataset_type
         self.data_source = data_source
         self.batch_size = batch_size
@@ -87,9 +160,11 @@ class GraphDataLoader():
         self.batch_iterator = self.get_batch()
       
     def __iter__(self):
+    """Function needed to be used as an itertor."""
         return self
 
     def __next__(self):
+    """Function needed to be used as an itertor."""
         with self.backend as backend:
             return self.batch_iterator.__next__()
         
--- ampligraph/datasets/sqlite_adapter.py
+++ ampligraph/datasets/sqlite_adapter.py
@@ -12,13 +12,32 @@ import numpy as np
 from urllib.request import pathname2url
 import os
 import shelve
-import pandas as pd
 from datetime import datetime
 from ampligraph.utils.profiling import get_human_readable_size
 
+
 DEFAULT_CHUNKSIZE = 30000
 class SQLiteAdapter():
+""" Class implementing database connection.
+
+    Example
+    -------
+    >>># with GraphDataLoader
+    >>>data = GraphDataLoader("data.csv", backend=SQLiteAdapter)
+    >>># raw
+    >>>backend = SQLiteAdapter("database.db")
+    >>>backend.populate("data.csv", dataset_type="train")
+"""
     def __init__(self, db_name, chunk_size=DEFAULT_CHUNKSIZE, verbose=False):
+    """ Initialise SQLiteAdapter.
+   
+        Parameters
+        ----------
+        db_name: name of the database.
+        chunk_size: size of a chunk to read data from while feeding the database,
+                    if not provided will be default (DEFAULT_CHUNKSIZE).
+        verbose: print status messages.
+    """
         self.db_name = db_name
         self.verbose = verbose
         self.indexed = False
@@ -30,6 +49,7 @@ class SQLiteAdapter():
             self.chunk_size = chunk_size
         
     def __enter__ (self):
+    """Context manager function to open or create if not exists database connection."""
         try:
             db_uri = 'file:{}?mode=rw'.format(pathname2url(self.db_name))
             self.connection = sqlite3.connect(db_uri, uri=True)
@@ -40,6 +60,8 @@ class SQLiteAdapter():
         return self
     
     def __exit__ (self, type, value, tb):
+    """Context manager exit function, required to used with "with statement", closes
+       the connection and do the rollback if required"""
         if tb is None:
             self.connection.commit()
             self.connection.close()
@@ -48,10 +70,13 @@ class SQLiteAdapter():
             self.connection.rollback()
         
     def _get_db_schema(self):
-        """Create 2 tables, one with indexes of objects and subjects in 
-            other words entities and the other with triples and indexes to 
-            navigate eaily on pairs subject-predicate, predicate-object."""
+    """Defines SQL queries to create a table with triples and indexes to 
+       navigate easily on pairs subject-predicate, predicate-object.
 
+       Returns
+       -------
+       db_schema: list of SQL commands to create tables and indexes.
+    """
         db_schema = [
         """CREATE TABLE triples_table (subject integer,
                                     predicate integer,
@@ -65,6 +90,12 @@ class SQLiteAdapter():
         return db_schema
 
     def _get_clean_up(self):
+    """Defines SQL commands to clean the databse (tables and indexes).
+
+       Returns
+       -------
+       clean_up: list of SQL commands to clean tables and indexes.
+    """  
         clean_up = ["drop index IF EXISTS triples_table_po_idx",
                     "drop index IF EXISTS triples_table_sp_idx",
                     "drop index IF EXISTS triples_table_type_idx",
@@ -72,6 +103,16 @@ class SQLiteAdapter():
         return clean_up
 
     def _execute_query(self, query):
+    """Connects to the database and execute given query.
+
+       Parameters
+       ----------
+       query: SQLite query to be executed.
+ 
+       Returns
+       -------
+       output: result of a query with fetchall().
+    """
         cursor = self.connection.cursor()
         output = None
         try:
@@ -85,10 +126,29 @@ class SQLiteAdapter():
         return output
 
     def _execute_queries(self, list_of_queries):
+    """Executes given list of queries one by one.
+
+       Parameters
+       ----------
+       query: list of SQLite queries to be executed.
+ 
+       Returns
+       -------
+       output: TODO! result of queries with fetchall().
+      
+    """
         for query in list_of_queries:
             self._execute_query(query)
 
     def _insert_values_to_a_table(self, table, values):
+    """Insert data into a given table in a database.
+
+       Parameters
+       ----------
+       table: table where to input data.
+       values: array of data with shape (N,3) to be written to the database, 
+               where N is a number of entries.      
+    """
         if self.verbose:
             print("inserting to a table...")
         if len(np.shape(values)) < 2:
@@ -109,9 +169,23 @@ class SQLiteAdapter():
         cursor.close()   
 
     def _create_database(self):
+    """Creates database."""
         self._execute_queries(self._get_db_schema())
 
     def get_triples(self, chunk, dataset_type="train"): 
+    """Get indexed triples.
+
+       Parameters
+       ----------
+       chunk: numpy array with a fragment of data of size (N,3), where each element is:
+              (subject, predicate, object).
+       dataset_type: defines what kind of data is it (train, test, validation).
+       
+       Returns
+       -------
+       tmp: numpy array of size (N,4) with indexed triples,
+            where each element is: (subject index, predicate index, object index, dataset_type).
+       """
         if self.verbose:
             print("getting triples...")
         with shelve.open(self.reversed_entities_shelf) as ents:
@@ -123,6 +197,17 @@ class SQLiteAdapter():
                 return np.append(tmp, np.array(len(chunk.values)*[dataset_type]).reshape(-1,1), axis=1)
 
     def index_entities_in_shelf(self):
+    """Index entities and relations. Creates shelves for mappings between
+       entities and relations to indexes and reverse mapping. 
+
+       Four shelves are created:
+       entities_shelf_<DATE>.shf - with map entities -> indexes
+       reversed_entities_shelf_<DATE>.shf - with map indexes -> entities
+       relations_shelf_<DATE>.shf - with map relations -> indexes
+       reversed_relations_shelf_<DATE>.shf - with map indexes -> relations
+
+       Rememer to use mappings for entities with entities and reltions with relations!
+    """
         if self.verbose:        
             print("indexing entities...")
         date = datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")
@@ -144,16 +229,17 @@ class SQLiteAdapter():
                             rels.update({str(key+ind):str(value) for key, value in enumerate(predicates)})                                                
 
     def index_entities(self):
-        """Get all keys from the shelf and populate database with them"""
+    """Index data. It reloads data before as it is an iterator."""
         self.reload_data()
-        self.index_entities_in_shelf() # remember to reinitialize data iterator before passing it through        
-        # TODO: iteratively add entities to the database tables from shelf
-        # maybe a flag letting know the state of the iterator (data source dried out!)
-        #print("Warning! Not yet done, TODO in place.")
-        #self._insert_values_to_a_table("entity_table", values_entities)
+        self.index_entities_in_shelf()
     
     def is_indexed(self):
-        """Check if shelves with indexes are set."""
+    """Check if shelves with indexes are set.
+    
+       Returns
+       -------
+       True/False - flag indicating whether indexing took place.
+    """
         if not hasattr(self, "entities_shelf"):
             return False
         if not hasattr(self, "reversed_entities_shelf"):
@@ -165,12 +251,23 @@ class SQLiteAdapter():
         return True
             
     def reload_data(self, verbose=False):
+    """Reinitialise an iterator with data."""
         self.data = self.loader(self.data_source, chunk_size=self.chunk_size)
         if verbose:
             print("Data reloaded", self.data)
         
     def populate(self, data_source, dataset_type="train", get_triples=None, loader=None):
-        """Condition: before you can enter triples you have to index data."""
+    """Condition: before you can enter triples you have to index data.
+
+       Parameters
+       ----------
+       data_source: file with data (e.g. csv file).
+       dataset_type: what type of data is it? (train | test | validation).
+       get_triples: function to obtain indexed triples.
+       loader: loading function to be used to load data, if None, the
+               DataSourceIdentifier will try to identify type and return
+               adequate loader.
+    """
         self.data_source = data_source        
         self.loader = loader
         if loader is None:
@@ -192,6 +289,17 @@ class SQLiteAdapter():
             print("data is populated")
     
     def get_size(self, table="triples_table", condition=""):
+    """Gets the size of the given table [with specified condition].
+
+       Parameters
+       ----------
+       table: table for which to obtain the size.
+       condition: condition to count only a subset of data.
+
+       Returns
+       -------
+       count: number of records in the table.
+    """
         query = "SELECT count(*) from {} {};".format(table, condition)
         count = self._execute_query(query)
         if count is None:
@@ -202,46 +310,68 @@ class SQLiteAdapter():
         return count[0][0]
 
     def clean_up(self):
+    """Clean the database."""
         status = self._execute_queries(self._get_clean_up())
         
     def remove_db(self):
+    """Remove the database file."""
         os.remove(self.db_name)        
         print("Database removed.")
 
     def _get_complementary_objects(self, triple):
+    """For a given triple retrive all triples whith same subjects and predicates.
+
+       Parameters
+       ----------
+       triple: list or array with 3 elements (subject, predicate, object).
+
+       Returns
+       -------
+       result of a query, list of objects.
+    """
         return self._execute_query("select {} union select distinct object from triples_table INDEXED BY \
                     triples_table_sp_idx where subject={} and predicate={}".format(triple[2], triple[0], triple[1]))
 
     def _get_complementary_subjects(self, triple):
+    """For a given triple retrive all triples whith same objects and predicates.
+
+       Parameters
+       ----------
+       triple: list or array with 3 elements (subject, predicate, object).
+
+       Returns
+       -------
+       result of a query, list of subjects.
+    """
+
         return self._execute_query("select {}  union select distinct subject from triples_table INDEXED BY \
                     triples_table_po_idx where predicate= {}  and object={}".format(triple[0], triple[1], triple[2]))
 
     def _get_complementary_entities(self, triple):
-        """returns the participating entities in the relation ?-p-o and s-p-?
+        """Returns the participating entities in the relation ?-p-o and s-p-?.
+
         Parameters
         ----------
-        x_triple : nd-array (3,)
-            triple (s-p-o) that we are querying
+        x_triple: nd-array (3,)
+            triple (s-p-o) that we are querying.
+
         Returns
         -------
-        ent_participating_as_objects : nd-array (n,1)
-            entities participating in the relation s-p-?
-        ent_participating_as_subjects : nd-array (n,1)
-            entities participating in the relation ?-p-o
+        entities: list of entities participating in the relations s-p-? and ?-p-o.
         """
         entities = self._get_complementary_objects(triple)
         entities.extend(self._get_complementary_subjects(triple))
         return list(set(entities))
     
-    def _get_batch(self, batch_size=1, dataset_type="train", use_filter=False):
+    def _get_batch(self, batch_size=1, dataset_type="train"):
         """Generator that returns the next batch of data.
 
         Parameters
         ----------
         dataset_type: string
-            indicates which dataset to use
-        batches_count: int
-            number of batches per epoch (default: -1, i.e. uses batch_size of 1)
+            indicates which dataset to use (train | test | validation).
+        batch_size: int
+            number of elements in a batch (default: 1).
         use_filter : bool
             Flag to indicate whether to return the concepts that need to be filtered
 
@@ -249,10 +379,8 @@ class SQLiteAdapter():
         -------
         batch_output : nd-array
             yields a batch of triples from the dataset type specified
-        participating_objects : nd-array [n,1]
-            all objects that were involved in the s-p-? relation. This is returned only if use_filter is set to true.
-        participating_subjects : nd-array [n,1]
-            all subjects that were involved in the ?-p-o relation. This is returned only if use_filter is set to true.
+        participating_entities : list of all entities that were involved in the s-p-? and ?-p-o relations. 
+                                 This is returned only if use_filter is set to true.
         """              
         query = "SELECT subject, predicate, object FROM triples_table INDEXED BY \
                                 triples_table_type_idx where dataset_type ='{}' LIMIT {}, {}"
@@ -265,18 +393,37 @@ class SQLiteAdapter():
             out = self._execute_query(query.format(dataset_type, i * batch_size, batch_size))
             if use_filter:
                 # get the filter values
-                participating_objects, participating_subjects = self.get_participating_entities(out)
-                yield out, participating_objects, participating_subjects
+                participating_entities = self.get_participating_entities(out)
+                yield out, participating_entities
             else:
                 yield out                    
                     
     def summary(self, count=True):
-        """Desiderata:
-            - does the database exists? +
-            - what tables does it have how many records, what are fields held and its types + example record? +
-            - any triggers?
-            - indexes declared?
-            - other?
+        """Prints summary of the database, whether it exists, what
+           tables does it have and how many records (count=True),
+           what are fields held and their types with an example record.
+
+           Parameters
+           ----------
+           count: whether to count number of records per table (can be time consuming)
+
+           Example
+           -------
+           >>>adapter = SQLiteAdapter("database_24-06-2020_03-51-12_PM.db")
+           >>>with adapter as db:
+           >>>    db.summary()
+           Summary for Database database_29-06-2020_09-37-20_AM.db
+           File size: 3.9453MB
+           Tables: triples_table
+           -------------
+           |TRIPLES_TABLE|
+           -------------
+           
+                  subject (int):   predicate (int):   object (int):   dataset_type (text(50)): 
+           e.g.   34321            29218              38102           train                     
+           
+           Records: 59070
+
         """
         if os.path.exists(self.db_name):
             print("Summary for Database {}".format(self.db_name))
@@ -308,5 +455,13 @@ class SQLiteAdapter():
             print("Database does not exist.")
             
     def _load(self, data_source, dataset_type="train"):
+    """Loads data from the data source to the database. Wrapper around populate method,
+       required by the GraphDataLoader interface.
+       
+       Parameters
+       ----------
+       data_source: file from where to read data (e.g. csv file).
+       dataset_type: kind of dataset that is being loaded (train | test | validation).
+    """
         self.data_source = data_source
         self.populate(self.data_source, dataset_type=dataset_type)
