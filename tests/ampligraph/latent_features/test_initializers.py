1338e82dfe14b7cf383d841e0d84b3d7ab7f3cc6
albernar <alberto.bernardi@yahoo.com>
2023-01-23 15:05:47 +0000

    copyright update

--- tests/ampligraph/latent_features/test_initializers.py
+++ tests/ampligraph/latent_features/test_initializers.py
@@ -1,3 +1,11 @@
+# Copyright 2019-2023 The AmpliGraph Authors. All Rights Reserved.
+#
+# This file is Licensed under the Apache License, Version 2.0.
+# A copy of the Licence is available in LICENCE, or at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
 from ampligraph.datasets import load_fb15k_237
 from ampligraph.latent_features import ScoringBasedEmbeddingModel
 import numpy as np
