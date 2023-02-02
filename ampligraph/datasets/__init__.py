93c39b6c9f480c71be0a03fd86ccabdb08242340
albernar <alberto.bernardi@yahoo.com>
2023-02-02 10:22:38 +0000

    clean up __all__

--- ampligraph/datasets/__init__.py
+++ ampligraph/datasets/__init__.py
@@ -9,19 +9,13 @@ from .datasets import load_from_csv, load_from_rdf, load_fb15k, load_wn18, load_
     load_yago3_10, load_wn18rr, load_wn11, load_fb13, load_onet20k, load_ppi5k, load_nl27k, load_cn15k, load_codex, \
     _load_xai_fb15k_237_experiment_log
 from .graph_data_loader import GraphDataLoader, DummyBackend, DataIndexer
-from .graph_partitioner import RandomVerticesGraphPartitioner, RandomEdgesGraphPartitioner, \
-    SortedEdgesGraphPartitioner, NaiveGraphPartitioner, DoubleSortedEdgesGraphPartitioner, BucketGraphPartitioner, \
-    EdgeBasedGraphPartitioner, AbstractGraphPartitioner, get_number_of_partitions, PARTITION_ALGO_REGISTRY
+from .graph_partitioner import BucketGraphPartitioner,PARTITION_ALGO_REGISTRY
 from .sqlite_adapter import SQLiteAdapter
-# from .partitioning_reporter import PartitioningReporter, compare_partitionings
 
-from .source_identifier import DataSourceIdentifier, load_csv, load_tar, load_gz, chunks
+from .source_identifier import DataSourceIdentifier, load_csv, load_tar, load_gz, load_json, chunks
 #
 #
-__all__ = ['load_from_csv', 'load_from_rdf', 'load_wn18', 'load_fb15k',
-           'load_fb15k_237', 'load_from_ntriples', 'load_yago3_10', 'load_wn18rr', 'load_wn11', 'load_fb13', 
+__all__ = ['load_from_csv', 'load_from_rdf', 'load_wn18', 'load_fb15k', 'load_fb15k_237',
+           'load_from_ntriples', 'load_yago3_10', 'load_wn18rr', 'load_wn11', 'load_fb13',
            'load_onet20k', 'load_ppi5k', 'load_nl27k', 'load_cn15k', 'load_codex',
-           'GraphDataLoader', 'RandomVerticesGraphPartitioner', 'RandomEdgesGraphPartitioner',
-           'SortedEdgesGraphPartitioner', 'NaiveGraphPartitioner', 'DoubleSortedEdgesGraphPartitioner',
-           'BucketGraphPartitioner', 'EdgeBasedGraphPartitioner', 'DataSourceIdentifier', 'SQLiteAdapter', 
-           'get_number_of_partitions', 'AbstractGraphPartitioner']
+           'GraphDataLoader', 'BucketGraphPartitioner']
