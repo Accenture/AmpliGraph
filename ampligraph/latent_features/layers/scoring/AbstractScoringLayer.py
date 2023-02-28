c7eb8fde6f3f72e325eaf01030c9e557704b4527
albernar <alberto.bernardi@yahoo.com>
2023-02-28 17:07:09 +0000

    solved hardcoded filtering index issue

--- ampligraph/latent_features/layers/scoring/AbstractScoringLayer.py
+++ ampligraph/latent_features/layers/scoring/AbstractScoringLayer.py
@@ -234,8 +234,6 @@ class AbstractScoringLayer(tf.keras.layers.Layer):
                         tf.cast(tf.gather(triple_score, [i]) <= score_filter, tf.int32))
                     # adjust the rank of the test triple accordingly
                     sub_rank = tf.tensor_scatter_nd_sub(sub_rank, [[i]], [num_filters_ranked_higher])
-                 
-                filter_index += 1
                 
             out_ranks = out_ranks.write(out_ranks.size(), sub_rank)
         
@@ -268,8 +266,10 @@ class AbstractScoringLayer(tf.keras.layers.Layer):
             
             if filters.shape[0] > 0:
                 for i in tf.range(tf.shape(triple_score)[0]):
-                    # TODO change the hard coded filter index
-                    filter_index = 1
+                    if corrupt_side in ["s", "o"] and filters.shape[0] == 1:
+                        filter_index = 0
+                    else:
+                        filter_index = 1
                     # get the ids of True positives that needs to be filtered
                     filter_ids = filters[filter_index][i]
                     
