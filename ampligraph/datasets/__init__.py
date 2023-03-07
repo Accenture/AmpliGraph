06973f83ea2879182585d5f5503762f2cdc47d23
Adrianna Janik <adrianna.janik@accenture.com>
2023-03-07 15:09:31 +0000

    Update init

--- ampligraph/datasets/__init__.py
+++ ampligraph/datasets/__init__.py
@@ -6,14 +6,16 @@
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 """Support for loading and managing datasets."""
-from .datasets import load_from_csv, load_from_rdf, load_fb15k, load_wn18, load_fb15k_237, load_from_ntriples, \
-    load_yago3_10, load_wn18rr, load_wn11, load_fb13, load_onet20k, load_ppi5k, load_nl27k, load_cn15k, load_codex, \
-    _load_xai_fb15k_237_experiment_log
+from .datasets import load_from_csv, load_from_rdf, load_fb15k, load_wn18,\
+        load_fb15k_237, load_from_ntriples, load_yago3_10, load_wn18rr,\
+        load_wn11, load_fb13, load_onet20k, load_ppi5k, load_nl27k,\
+        load_cn15k, load_codex, _load_xai_fb15k_237_experiment_log
 from .graph_data_loader import GraphDataLoader, NoBackend, DataIndexer
 from .graph_partitioner import BucketGraphPartitioner, PARTITION_ALGO_REGISTRY
 from .sqlite_adapter import SQLiteAdapter
 
-from .source_identifier import DataSourceIdentifier, load_csv, load_tar, load_gz, load_json, chunks
+from .source_identifier import DataSourceIdentifier, load_csv, load_tar,\
+        load_gz, load_json, chunks
 
 
 __all__ = [
